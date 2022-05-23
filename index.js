f847861f6e8c57cbda835d929467e8d7e57d745f
Swati <swatic019@gmail.com>
2022-05-23 23:30:40 +0530

    added alert message

--- index.js
+++ index.js
@@ -8,28 +8,55 @@ function Book(name, author, type) {
 }
 
 // Display Constructor
- 
+
 function Display() {
 
 }
 
 // Add methods to display prototype
-Display.prototype.add = function(book){
+Display.prototype.add = function (book) {
     console.log('Adding');
+    tableBody = document.getElementById('tableBody');
+    let uiString = `<tr>
+                        <td>${book.name}</td>
+                        <td>${book.author}</td>
+                        <td>${book.type}</td>
+                    </tr>`;
+    tableBody.innerHTML += uiString;
+    // book.preventDefault();
 }
-Display.prototype.clear = function(){
+
+Display.prototype.clear = function () {
     let libraryForm = document.getElementById('libraryForm');
     libraryForm.reset();
 }
-
-
+Display.prototype.validate = function (book) {
+    if (book.name.length < 2 || book.author.length < 2) {
+        return false
+    }
+    else {
+        return true;
+    }
+}
+Display.prototype.show = function (type, displayMessage) {
+    let message = document.getElementById('message');
+    message.innerHTML = `<div class="alert alert-${type} alert-dismissible fade show" role="alert">
+                            <strong>Message: </strong> ${displayMessage}
+                            <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+                        </div>`;
+     setTimeout(function() {
+         message.innerHTML = ""
+      }, 2000);
+                                    }
 
 // Add submit event listener to libraryForm
 
 let libraryForm = document.getElementById('libraryForm');
 libraryForm.addEventListener("submit", libraryFormSub);
 
+
 function libraryFormSub(e) {
+    e.preventDefault();
     console.log('You have submitted library form');
     let name = document.getElementById('bookname').value;
     let author = document.getElementById('author').value;
@@ -38,7 +65,7 @@ function libraryFormSub(e) {
     let fiction = document.getElementById('fiction');
     let programming = document.getElementById('programming');
     let cooking = document.getElementById('cooking');
-    
+
 
     if (fiction.checked) {
         type = fiction.value;
@@ -54,11 +81,19 @@ function libraryFormSub(e) {
     console.log(book);
 
     let display = new Display();
-    display.add(book);
-    display.clear();
-    
-   e.preventDefault();
- 
-} 
+
+    if (display.validate(book)) {
+
+        display.add(book);
+        display.clear();
+        display.show('success', 'your book has been sucessfully added')
+    }
+    else {
+        display.show('danger', 'Sorry you can not add this book');
+    }
+
+
+
+}
 
 
